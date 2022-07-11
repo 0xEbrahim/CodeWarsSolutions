038a4254925ef7523cbdb7d4165c4b3f8a990f5c
Ebrahim El-Sayed Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-07-12 01:14:46 +0200

    Create Calculate BMI.js

---
+++ Calculate BMI.js
@@ -0,0 +1,11 @@
+function bmi(weight, height) {
+  let BMI=weight/height**2;
+  if(BMI <= 18.5)
+    return `Underweight`;
+  else if(BMI <= 25)
+    return `Normal`;
+  else if(BMI <= 30)
+    return `Overweight`;
+  else
+    return `Obese`;
+}
